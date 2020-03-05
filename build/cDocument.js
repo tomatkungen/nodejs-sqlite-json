bebe6df30c09cb4a9b7bc31d590f5c57b15da016
Tomatkungen <mr_a2@hotmail.com>
2020-03-05 19:20:36 +0100

    fix: regenerate build

--- build/cDocument.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var cDocument = (function () {
-    function cDocument() {
-    }
-    return cDocument;
-}());
-exports.cDocument = cDocument;
